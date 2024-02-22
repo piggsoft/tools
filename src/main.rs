21ff5377afed5d1f064b7d09b9b318748f452bda
piggsoft <piggsoft@163.com>
2024-02-22 17:10:06 +0800

    修改命令写法和解决borrow问题

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,51 @@
-use clap::{arg, command, Parser};
+use std::path::PathBuf;
+use clap::{arg, command, Parser, Subcommand};
 
 mod make_link;
 
+
 #[derive(Parser)] // requires `derive` feature
 #[command(name = "tools", bin_name = "tools", version = "0.1.0", about = "Piggsoft的工具包")]
-enum ToolsCli {
-    #[command(name = "makeLink", version="0.1.0", about = "将<source>文件迁移到<target>，并将<source>变为软链接")]
-    MakeLink(MakeLinkArgs),  //subcommand
+struct ToolsCli {
+    name: Option<String>,
+    #[arg(short, long, value_name = "FILE")]
+    config: Option<PathBuf>,
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
 
-    #[command(name = "Host", about = "打印host")]
-    Host(MakeLinkArgs),  //subcommand
+#[derive(Subcommand)]
+enum Commands {
+    #[command(name = "makeLink", version = "0.1.0", about = "将<source>文件迁移到<target>，并将<source>变为软链接")]
+    MakeLink(MakeLinkArgs),
 }
 
 
-#[derive(clap::Args)]
+#[derive(clap::Args, Debug)]
 struct MakeLinkArgs {
     #[arg(required = true, long, short = 's', help = "原始文件或者目录")]
     source: String,
-    #[arg(required = false, long, short = 't', default_value = "", help = "迁移的目标文件或者目录，保证在迁移前无该文件或目录", long_help = "不传时，将使用<source_path>，并将其开头的<C:>改成<D:>")]
-    target: String,
+    #[arg(required = false, long, short = 't', help = "迁移的目标文件或者目录，保证在迁移前无该文件或目录", long_help = "不传时，将使用<source_path>，并将其开头的<C:>改成<D:>")]
+    target: Option<String>,
 }
 
 
 fn main() {
-    match ToolsCli::parse() {
-        ToolsCli::MakeLink(args) => make_link::make_link(args.source.as_str(), args.target.as_str()),
-        _ => println!("不支持的命令"),
+    // match ToolsCli::parse() {
+    //     ToolsCli::MakeLink(args) => make_link::make_link(args.source.as_str(), args.target.as_str()),
+    //     _ => println!("不支持的命令"),
+    // }
+
+    let cli = ToolsCli::parse();
+
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Some(Commands::MakeLink(args)) => {
+            make_link::make_link(&args.source, &args.target);
+        },
+        None => {}
     }
 }
--- src/make_link.rs
+++ src/make_link.rs
@@ -1,66 +1,5 @@
 use std::{io, process};
 
-pub(crate) fn make_link(source: &str, target: &str) {
-    let source_path = &source[..];
-    let binding = source
-        .replace("C:\\", "D:\\")
-        .replace("c:\\", "d:\\")
-        .replace("C:/", "D:/")
-        .replace("c:/", "d:/");
-    let mut target_path = &binding[..];
-    if target != "" {
-        target_path = &target[..];
-    }
-    println!("原始目录为：{source_path}");
-    println!("软连接迁移后的目录为：{target_path}");
-    println!("请确认是否将原始目录: {source_path}，迁移到：{target_path}，并将原始目录删除后建立软连接？ Y or N ？");
-
-    let mut user_input: String = String::new();
-    io::stdin()
-        .read_line(&mut user_input)
-        .expect("读取输入出错！");
-
-
-    if user_input.trim().to_ascii_uppercase().as_str() != "Y" {
-        println!("程序正在退出！");
-        return;
-    }
-
-    println!("---------------开始目录拷贝---------------");
-    let result = copy_dir::copy_dir(source_path, target_path);
-    match result {
-        Ok(errors) => {
-            if !errors.is_empty() {
-                errors.iter().for_each(|err| { eprintln!("目录拷贝出错: {}", err) });
-                process::exit(-1);
-            }
-        },
-        Err(err) => {
-            eprintln!("目录拷贝出错: {:?}", err);
-            process::exit(-1);
-        },
-    }
-    println!("---------------结束目录拷贝---------------");
-
-    println!("---------------开始原始目录删除---------------");
-    let result = rm_rf::ensure_removed(source_path);
-    if let Err(err) = result {
-        eprintln!("目录删除出错: {}", err);
-        process::exit(-1);
-    }
-    println!("---------------结束原始目录删除---------------");
-
-    println!("---------------开始建立软链接---------------");
-    let result = symlink::symlink_auto(target_path, source_path);
-    if let Err(err) = result {
-        eprintln!("建立软链接出错: {}", err);
-        process::exit(-1);
-    }
-    println!("---------------结束建立软链接---------------");
-
-    println!("---------------Make Link Done!---------------");
-}
-
 // fn cmd() -> clap::Command {
 //     command!() // requires `cargo` feature
 //         .subcommand_required(true)
@@ -80,3 +19,68 @@ pub(crate) fn make_link(source: &str, target: &str) {
 //             )
 //         )
 // }
+
+fn replace_path_prefix(source: &String) -> String {
+    source
+        .replace("C:\\", "D:\\")
+        .replace("c:\\", "d:\\")
+        .replace("C:/", "D:/")
+        .replace("c:/", "d:/")
+}
+
+pub(crate) fn make_link(source: &String, target: &Option<String>) {
+
+    let replace_source_prefix = replace_path_prefix(&source);
+    let target_path = target.as_ref().unwrap_or(&replace_source_prefix);
+    let source_path = source;
+
+
+    println!("原始目录为：{source_path}");
+    println!("软连接迁移后的目录为：{target_path}");
+    println!("请确认是否将原始目录: {source_path}，迁移到：{target_path}，并将原始目录删除后建立软连接？ Y or N ？");
+
+    let mut user_input: String = String::new();
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("读取输入出错！");
+
+
+    if user_input.trim().to_ascii_uppercase().as_str() != "Y" {
+        println!("程序正在退出！");
+        return;
+    }
+
+    println!("---------------[1/3]开始目录拷贝---------------");
+    let result = copy_dir::copy_dir(&source_path, &target_path);
+    match result {
+        Ok(errors) => {
+            if !errors.is_empty() {
+                errors.iter().for_each(|err| { eprintln!("目录拷贝出错: {}", err) });
+                process::exit(-1);
+            }
+        }
+        Err(err) => {
+            eprintln!("目录拷贝出错: {:?}", err);
+            process::exit(-1);
+        }
+    }
+    println!("---------------[1/3]结束目录拷贝---------------");
+
+    println!("---------------[2/3]开始原始目录删除---------------");
+    let result = rm_rf::ensure_removed(&source_path);
+    if let Err(err) = result {
+        eprintln!("目录删除出错: {}", err);
+        process::exit(-1);
+    }
+    println!("---------------[2/3]结束原始目录删除---------------");
+
+    println!("---------------[3/3]开始建立软链接---------------");
+    let result = symlink::symlink_auto(&target_path, &source_path);
+    if let Err(err) = result {
+        eprintln!("建立软链接出错: {}", err);
+        process::exit(-1);
+    }
+    println!("---------------[3/3]结束建立软链接---------------");
+
+    println!("---------------恭喜!完成软链接创建!---------------");
+}

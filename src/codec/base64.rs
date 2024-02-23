b166e450cdf49e2bf84c1a47fad9fdf3267b6ba5
piggsoft <piggsoft@163.com>
2024-02-23 12:20:43 +0800

    添加base64加解密

--- src/codec/base64.rs
+++ src/codec/base64.rs
@@ -1,13 +1,21 @@
+use base64::{Engine as _, engine::general_purpose::STANDARD};
+
 use super::{CodecImplArgs, common};
 
 pub struct Base64Codec {}
 
 impl common::Codec for Base64Codec {
     fn encode(&self, codec_args: &CodecImplArgs) -> Result<String, common::CodecError> {
-        todo!()
+        Ok(STANDARD.encode(&codec_args.input))
     }
 
     fn decode(&self, codec_args: &CodecImplArgs) -> Result<String, common::CodecError> {
-        todo!()
+        match STANDARD.decode(&codec_args.input) {
+            Ok(t) => match String::from_utf8(t) {
+                Ok(s) => Ok(s),
+                Err(e) => Err(common::CodecError { message: e.to_string() })
+            },
+            Err(e) => Err(common::CodecError { message: e.to_string() })
+        }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,2 @@
 pub mod make_link;
-pub mod url_encoding;
 pub mod codec;
